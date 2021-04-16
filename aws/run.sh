267ff20977e23843528e8a5e41eeed66c09abbaa
Semyon Kolesnikov <skolesnikov@mirantis.com>
2021-04-16 11:59:56 +0300

    Added run.sh to start clustser

--- aws/run.sh
+++ aws/run.sh
@@ -1,32 +1,7 @@
 #!/usr/bin/env sh
 
-# run.sh may run from any dir with full path to it
-# Doesn't matter where we are running
-# It wants to know for which labname/caseid it can be ran
-# You need to specify arguments for CaseID running dir
-
-read -rp "Write CaseID or another Lab Name: " LABNAME
-echo .....................................................
-echo "$LABNAME"
-echo .....................................................
-while true; do
-    read -rp "Do you want to use current dir to save state files? " yn
-    case $yn in
-        [Yy]* ) LAUNCHPAD_LABS_DIR=$(pwd); break;;
-        [Nn]* ) read -rp "Please type path where to save state files: " LAUNCHPAD_LABS_DIR; break;;
-        * ) echo "Please answer Yes or No.";;
-    esac
-done
-
-echo "Using this directory: $LAUNCHPAD_LABS_DIR"
-
-# Create catalog where we are working
-
-
-docker image pull lotarc/launchpadlabs
-docker run -it --rm --name "$LABNAME" \
--v ~/.aws:/home/root/.aws \
--v "$LAUNCHPAD_LABS_DIR":/launchpadlabs/terraform.tfvars \
--v "$LAUNCHPAD_LABS_DIR":/launchpadlabs/terraform.tfstate \
--v "$LAUNCHPAD_LABS_DIR":/launchpadlabs/launchpad.yaml \
-launchpadlabs:latest
+terraform init && terraform plan
+terraform apply --auto-approve -compact-warnings
+terraform output --raw mke_cluster > ./launchpad.yaml
+launchpad describe config
+launchpad apply
